0ec3f7ff0114effebdbaec2f80e3f3e1dca1ae57
kaansonmezoz <ksonmezoz@outlook.com>
2020-06-15 20:20:50 +0300

    Created chess pieces. Created only their skeletons

---
+++ game/pieces/chess_piece.py
@@ -0,0 +1,52 @@
+from abc     import ABCMeta, abstractmethod
+from .bishop import Bishop
+from .king   import King
+from .knight import Knight
+from .pawn   import Pawn
+from .queen  import Queen
+from .rook   import Rook
+
+
+class ChessPiece(metaclass=ABCMeta):
+    def __init__(self, name, color, x, y):
+        self._name = name
+        self._color = color
+        self._x = x
+        self._y = y
+
+    def get_id(self):
+        return self._color + "_" + self._name
+
+    @abstractmethod
+    def can_move(self):
+        ##TODO: should be implemented
+        pass
+
+    @abstractmethod
+    def move(self):
+        ##TODO: should be implemented
+        pass
+
+    @staticmethod
+    def bishop(name, color, x, y):
+        return Bishop(name, color, x, y)
+
+    @staticmethod
+    def king(name, color, x, y):
+        return King(name, color, x, y)
+
+    @staticmethod
+    def knight(name, color, x, y):
+        return Knight(name, color, x, y)
+    
+    @staticmethod
+    def pawn(name, color, x, y):
+        return Pawn(name, color, x, y)
+
+    @staticmethod
+    def queen(name, color, x, y):
+        return Queen(name, color, x, y)
+
+    @staticmethod
+    def rook(name, color, x, y):
+        return Rook(name, color, x, y)
---
+++ game/pieces/pawn.py
@@ -0,0 +1,13 @@
+from .chess_piece import ChessPiece
+
+class Pawn(ChessPiece):
+    def __init__(self, name, color, x, y):
+        super().__init__(name, color, x, y)
+    
+    def can_move(self):
+        ##TODO: should be implemented
+        return
+
+    def move(self):
+        ##TODO: should be implemented
+        return
