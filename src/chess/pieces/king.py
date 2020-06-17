420a54a913d68bfd5a99cb2e763cf2fedb128874
kaansonmezoz <ksonmezoz@outlook.com>
2020-06-17 19:33:56 +0300

    Added todo

--- src/chess/pieces/king.py
+++ src/chess/pieces/king.py
@@ -4,7 +4,7 @@ print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resol
 from src.chess.pieces.chess_piece  import ChessPiece
 from src.chess.chess_board import ChessBoard
 
-class King(ChessPiece):
+class King(ChessPiece): # rok yapabilmesiyle ilgili bir state'i olmasi lazim. ayrica rok yaptigi zaman gerekli duzenlemelerin de yapilmasi gerekiyor. Boardda
     def __init__(self, piece_type, color, number, x, y):
         super().__init__(piece_type, color, number, x, y)
     
