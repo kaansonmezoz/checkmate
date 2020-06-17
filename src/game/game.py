19747b9f5b50c05238c9ab1706345570760e9191
kaansonmezoz <ksonmezoz@outlook.com>
2020-06-17 17:16:14 +0300

    Removed unnecessary comments

--- src/game/game.py
+++ src/game/game.py
@@ -1,12 +1,10 @@
 from pathlib import Path
 print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resolve())
 
-#from . import Renderer
 from src.game.game_screen import GameScreen
 
 class Game:
     def __init__(self, chess_board):
-        #self.__renderer = Renderer(pygame)        
         self.__game_screen = GameScreen()
         self.__chess_board = chess_board    
 
