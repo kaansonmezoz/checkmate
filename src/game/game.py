0a6f60f2650d424657976c693034f6fbb16034df
kaansonmezoz <ksonmezoz@outlook.com>
2020-06-17 17:22:51 +0300

    Rendered initial state of chess board

--- src/game/game.py
+++ src/game/game.py
@@ -6,7 +6,8 @@ from src.game.game_screen import GameScreen
 class Game:
     def __init__(self, chess_board):
         self.__game_screen = GameScreen()
-        self.__chess_board = chess_board    
+        self.__chess_board = chess_board
+        self.__game_screen.update(self.__chess_board.white_pieces(), self.__chess_board.black_pieces())
 
 # burada oyun icin gerekli diger dependecyler her sey yaratilmali
 # suan her class kendi dependencysisini yaratiyor onu kaldırmak lazim
