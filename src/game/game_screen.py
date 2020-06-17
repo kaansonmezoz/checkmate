8a8acd744708a3c9daf1252d7a28892d394f9c2a
kaansonmezoz <ksonmezoz@outlook.com>
2020-06-17 14:01:31 +0300

    Updated game screen and added todos

--- src/game/game_screen.py
+++ src/game/game_screen.py
@@ -3,22 +3,38 @@ print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resol
 
 from src.game.game_images import GameImages
 
+import pygame
+from pygame.locals import *
+
 class GameScreen:
-    def __init__(self, pygame, chess_board):
+    def __init__(self):
         # pygame.error: No video mode has been set hatası aldim.
         # bunu cozmek icin basta sallamasyon bir display mode olusturdum.
 
+        # ekrana bastirilmis olan taslar da burada olsun (baslangic_x, baslangic_y) seklinde tutularak
+        # bu sayede biri ekranda basinca bir seye direkt olarak ilgili tasi bulabilelim.
+        # o ilgili tasi da board'a geceriz ve orada ilgili islemlerin yapilmasini saglariz tasin hareketi gibi
+        # ama bu durumda da bu hareket bitince o tasin image'nin bulundugu konumu silmek gerekecek
+        # bir de cok fazla data share edilmis olacak bu fikirden caydim.
+        # en temizi board'dan yerlerini alalim taslarin sonra da gidip image'larını aliriz
+        # dolayısıyla buradaki chess_piece_image arkada board üzerinde bir chess_piece'e denk gelsin
+        # ya da sürekli olarak burada image ekleyelim verilen koordinata diger secenek de bu
+
+
         self.__pygame = pygame
+        self.__pygame.init()
         self.__screen = pygame.display.set_mode((600,600))
-        self.__chess_board = chess_board
         self.__game_images = GameImages(pygame)        
         self.__init_screen()
         ## blit sadece cizdiriyor ekrani.
         self.__screen.blit(self.__game_images.board(),(0,0))
-        # butun degisiklikleri vs renderlamak icin bunu yapmak lazim yani update'i cagirtmak lazim
+
+        ## Oyun tahtasindaki karenin boyutları
+        self.__square_height = 0 # boarddaki bir karenin yuksekligi
+        self.__square_width = 0  # boarddaki bir karenin genisligi
+
+        # butun degisiklikleri vs renderlamak icin bunu yapmak lazim yani update'i cagirtmak lazim        
         pygame.display.update() 
-
-
     
     def __init_screen(self):        
         size = self.__game_images.board_size()
@@ -27,3 +43,27 @@ class GameScreen:
         screen.blit(self.__game_images.board(), (0, 0))
         self.__screen = screen
 
+    def update(self, white_pieces, black_pieces):
+        # bütün taslarin konumlari alinmali
+        # once board uzerinde bu taslar konulmalı
+        # sonrasında da bunları renderlamak gerekiyor.
+
+        # Daha iyi bir cozum lazim
+
+        self.__clear_screen()
+        self.__screen.blit(self.__game_images.board(), (0, 0))
+
+        self.__put_pieces('white', white_pieces, self.__square_width, self.__square_height)
+        self.__put_pieces('black', black_pieces, self.__square_width, self.__square_height)
+
+        self.__pygame.display.update()
+    
+    def __clear_screen(self):
+        black = (0, 0, 0)
+        self.__screen.fill(black)
+    
+    def __put_pieces(self, color, pieces, square_width, square_height):
+        for piece in pieces:
+            location = (piece.x() * square_width, piece.y() * square_height)
+            image = self.__game_images.piece_image(color, piece.get_type())
+            self.__screen_blit(image, location)
