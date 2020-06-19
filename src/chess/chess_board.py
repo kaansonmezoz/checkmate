f8a1a43e4c84bb9a526f86ef6aa8edc409ebf21f
kaansonmezoz <ksonmezoz@outlook.com>
2020-06-19 14:14:21 +0300

    Changed axis variables.    - y is height (i.e y is used as i)    - x is width  (i.e x is used as j)

--- src/chess/chess_board.py
+++ src/chess/chess_board.py
@@ -20,21 +20,21 @@ class ChessBoard:
     def __put_black_chess_pieces(self, black_pieces):
         self.__put_chess_pieces(black_pieces, 'black', 7, 6)
 
-    def __put_chess_pieces(self, pieces, color, x1, x2):
-        self.__board[x1][0] = self.__create_chess_piece(pieces, ChessPiece.rook, 'rook', color,'1', x1, 0)
-        self.__board[x1][1] = self.__create_chess_piece(pieces, ChessPiece.knight, 'knight', color, '1', x1, 1)
-        self.__board[x1][2] = self.__create_chess_piece(pieces, ChessPiece.bishop, 'bishop', color, '1', x1, 2)
-        self.__board[x1][3] = self.__create_chess_piece(pieces, ChessPiece.queen, 'queen', color, '1', x1, 3)
-        self.__board[x1][4] = self.__create_chess_piece(pieces, ChessPiece.king, 'king', color, '1' ,x1, 4)
-        self.__board[x1][5] = self.__create_chess_piece(pieces, ChessPiece.bishop, 'bishop', color, '2', x1, 5)
-        self.__board[x1][6] = self.__create_chess_piece(pieces, ChessPiece.knight, 'knight', color, '2', x1, 6)
-        self.__board[x1][7] = self.__create_chess_piece(pieces, ChessPiece.rook, 'rook', color, '2', x1, 7)
+    def __put_chess_pieces(self, pieces, color, y1, y2):
+        self.__board[y1][0] = self.__create_chess_piece(pieces, ChessPiece.rook, 'rook', color,'1', y1, 0)
+        self.__board[y1][1] = self.__create_chess_piece(pieces, ChessPiece.knight, 'knight', color, '1', y1, 1)
+        self.__board[y1][2] = self.__create_chess_piece(pieces, ChessPiece.bishop, 'bishop', color, '1', y1, 2)
+        self.__board[y1][3] = self.__create_chess_piece(pieces, ChessPiece.queen, 'queen', color, '1', y1, 3)
+        self.__board[y1][4] = self.__create_chess_piece(pieces, ChessPiece.king, 'king', color, '1' ,y1, 4)
+        self.__board[y1][5] = self.__create_chess_piece(pieces, ChessPiece.bishop, 'bishop', color, '2', y1, 5)
+        self.__board[y1][6] = self.__create_chess_piece(pieces, ChessPiece.knight, 'knight', color, '2', y1, 6)
+        self.__board[y1][7] = self.__create_chess_piece(pieces, ChessPiece.rook, 'rook', color, '2', y1, 7)
 
-        for y in range(8):
-            self.__board[x2][y] = self.__create_chess_piece(pieces, ChessPiece.pawn, 'pawn', color, str(y+1), x2, y)
+        for x in range(8):
+            self.__board[y2][x] = self.__create_chess_piece(pieces, ChessPiece.pawn, 'pawn', color, str(x+1), y2, x)
 
-    def __create_chess_piece(self, pieces, creator, piece_type, color, number, x, y):
-        piece = creator(piece_type, color, number, x, y)
+    def __create_chess_piece(self, pieces, creator, piece_type, color, number, y, x):
+        piece = creator(piece_type, color, number, y, x)
         pieces.append(piece)
         return piece
 
